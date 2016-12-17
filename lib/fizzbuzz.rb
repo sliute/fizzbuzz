bdfb239c0b5e50a9924b932aea55db178e7125ca
Stefan Liute <stefanliute@gmail.com>
2016-12-17 16:04:39 +0000

    Second pair programming final run

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -10,3 +10,13 @@ def fizzbuzz(number)
     number
   end
 end
+
+=begin
+#running fizzbuzz for the 1..100 range in irb:
+1.upto(100) { |x| puts fizzbuzz(x) }
+=end
+
+=begin
+# run fizzbuzz on a single line file:
+p (1..100).map { |i| a = "#{"fizz" if i % 3 == 0}#{"buzz" if i % 5 ==0}" ; a.empty? ? i : a }
+=end
